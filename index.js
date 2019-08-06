0c1bae5374b8a4799ce539e305e797f5468c817f
Nate Magera <nmagera07@gmail.com>
2019-08-06 15:09:06 -0400

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const Posts = require('./data/db')
+
+const Comments = require('./data/db')
+
+const server = express()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const port = 8000
+server.listen(port, () => console.log('hello you are connected'))
